cd2059d8126fe51e06d7a554ebb0f985f61a27ca
Giully314 <giully@protonmail.com>
2025-04-09 17:43:57 +0200

    end chapter 4.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,10 +6,40 @@ import (
 	"time"
 
 	"github.com/Giully314/greenlight/internal/data"
+	"github.com/Giully314/greenlight/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create movie")
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+
+	// Body is closed by go http server.
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	data.ValidateMovie(v, movie)
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 // End point: "GET /v1/movies/:id"
